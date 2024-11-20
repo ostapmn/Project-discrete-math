8a3742f44c78e9678adaac580dd543169ae1813c
Ostap Mnykh <mnykh.pn@ucu.edu.ua>
2024-11-20 15:06:49 +0200

    First try

---
+++ main.py
@@ -0,0 +1,33 @@
+"""Module algorithm"""
+
+# PR(A) = (1-d) + d (PR(T1)/C(T1) + ... + PR(Tn)/C(Tn))
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+edge_list = [('A', 'B'), ('B', 'D'), ('A', 'C'), ('C', 'D'), ('C', 'B'), ('C', 'A'), ('D', 'C')]
+
+graph = nx.DiGraph()
+graph.add_edges_from(edge_list)
+
+ITERATIONS = len(graph.nodes)
+START_RANK = 1 / ITERATIONS
+
+page_rank = {key: START_RANK for key in graph.nodes}
+page_prev = {key: 0 for key in graph.nodes}
+
+
+for _ in range(ITERATIONS-2):
+    for node in page_rank:
+        current_rank = 0
+        for el in graph.edges:
+            if el[1] == node:
+                out_links = graph.out_degree(el[0])
+                current_rank += page_rank[el[0]]/out_links
+        page_prev[node] = current_rank
+    page_rank = dict(page_prev.items())
+
+page_rank = dict(sorted(page_rank.items(), key = lambda x: -x[1]))
+
+nx.draw_circular(graph, with_labels = True)
+plt.show()
