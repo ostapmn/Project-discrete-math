6b50c4a3794b6e6b36db043dd6261fa747ac3cf0
Yustyna Hayevska <justya200711@gmail.com>
2024-11-29 22:06:48 +0200

    Ganged lists to tuples

--- yustya_test.py
+++ yustya_test.py
@@ -7,10 +7,10 @@ def pages_directions(pages):
     for key, values in pages.items():
         if len(values) == 1:
             values = str(values).lstrip("['").rstrip("']")
-            pages_list.append([key, values])
+            pages_list.append((key, values))
         elif len(values) > 1:
             for one_val in values:
-                pages_list.append([key, one_val])
+                pages_list.append((key, one_val))
     pages_list = sorted(pages_list)
     return pages_list
 
