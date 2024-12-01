2becd16b07417ca2755d0ac36c362241336e08ac
Ostap Mnykh <mnykh.pn@ucu.edu.ua>
2024-12-01 17:49:46 +0200

    Merge

--- main.py
+++ main.py
@@ -5,15 +5,17 @@
 import unicodedata
 import re
 import os
+from collections import deque
+from functools import lru_cache
 import networkx as nx
 import matplotlib.pyplot as plt
-from collections import deque
 from pdfminer.high_level import extract_text # lib to work with pdf files
 
 PATTERN = re.compile(r'\[(.+?)\]\s+([\w.,\s&]+?),\s+(.*?),\s+(.*?)(?:\.|\n)') # pattern to identify paper name
 
-edge_list = [('A', 'B'), ('B', 'D'), ('A', 'C'), ('C', 'D'), ('C', 'B'), ('C', 'A'), ('D', 'C')]
+# edge_list = [('A', 'B'), ('B', 'D'), ('A', 'C'), ('C', 'D'), ('C', 'B'), ('C', 'A'), ('D', 'C')]
 
+@lru_cache
 def normalize_text(text):
     """Function to convert 'unique' elements to normal one"""
     normalized = unicodedata.normalize("NFKD", text)
@@ -33,7 +35,7 @@ def read_pdf(file_name: str, graph = None) -> list:
     folder, paper_name = file_name.split('/') # we assume that all our files in one directory
     if file_name[:-4] in graph.keys():
         return graph
-    
+
     text = extract_text(file_name) #extracting text from pdf
     start_point = text.find('References\n') # starting point from wear to parse the text
     matches = re.findall(PATTERN, normalize_text(text[start_point:])) # finding matches
@@ -50,6 +52,23 @@ def read_pdf(file_name: str, graph = None) -> list:
     return graph
 
 # print(read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf'))
+
+#===Pages directions===#
+
+def pages_directions(pages: dict) -> list[tuple[str]]:
+    """
+    Converts inout dict into a list of list with a links directions.
+    """
+    pages_list = []
+    for key, values in pages.items():
+        if len(values) == 1:
+            values = str(values).lstrip("['").rstrip("']")
+            pages_list.append((key, values))
+        elif len(values) > 1:
+            for one_val in values:
+                pages_list.append((key, one_val))
+    pages_list = sorted(pages_list)
+    return pages_list
 
 def bfs(graph: dict, start: str) -> list:
     """
@@ -71,20 +90,21 @@ def bfs(graph: dict, start: str) -> list:
 
     return visited
 
+# print(pages_directions(
+#     read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf')))
+
+graph = nx.DiGraph()
+graph.add_edges_from(pages_directions(
+    read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf')))
+
+
 ITERATIONS = len(graph.nodes)
 START_RANK = 1 / ITERATIONS
 EPSILON = 0.01
 DAMPING_FACTOR = 0.85
 
-graph_rank = nx.DiGraph()
-graph_rank.add_edges_from(edge_list)
-
-
-page_rank = {key: START_RANK for key in graph_rank.nodes}
-page_prev = {key: 0 for key in graph_rank.nodes}
-
-
-# diff = [abs(page_prev[node] - page_rank[node]) < EPSILON for node in graph_rank.nodes]
+page_rank = {key: START_RANK for key in graph.nodes}
+page_prev = {key: 0 for key in graph.nodes}
 
 while True:
     for node in page_rank:
@@ -100,14 +120,14 @@ while True:
 
         page_prev[node] = (1 - DAMPING_FACTOR) + DAMPING_FACTOR * current_rank
 
-    diff = sum(abs(page_prev[node] - page_rank[node]) for node in graph.nodes)
+    diff = [abs(page_prev[node] - page_rank[node])<EPSILON for node in graph.nodes]
 
-    if diff < EPSILON:
+    if all(diff):
         break
 
+    page_rank = dict(page_prev.items())
 
 page_rank = dict(sorted(page_rank.items(), key = lambda x: -x[1]))
 print(page_rank)
-
-nx.draw_circular(graph_rank, with_labels = True)
+nx.draw_circular(graph, with_labels = True)
 plt.show()
--- test_network.py
+++ test_network.py
@@ -1,25 +1,66 @@
-"""Module for testing lib functions"""
+"""Module algorithm"""
 
-import re
-import unicodedata
-from pdfminer.high_level import extract_text
+# PR(A) = (1-d) + d * (PR(T1)/C(T1) + ... + PR(Tn)/C(Tn))
 
-PATTERN = re.compile(r'\[\d*?\]')
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import deque
 
-def normalize_text(text):
-    normalized = unicodedata.normalize("NFKD", text)
-    return normalized
+edge_list = [('acharacterizationoffibrantsegalcategories', 'threemodelsforthehomotopytheoryofhomotopytheories'), ('higherandderivedstacksaglobaloverview', 'acharacterizationoffibrantsegalcategories'), ('higherandderivedstacksaglobaloverview', 'threemodelsforthehomotopytheoryofhomotopytheories'), ('threemodelsforthehomotopytheoryofhomotopytheories', 'acharacterizationoffibrantsegalcategories'), ('threemodelsforthehomotopytheoryofhomotopytheories', 'threemodelsforthehomotopytheoryofhomotopytheories')]
 
-x = extract_text('test_papers/higherandderivedstacksaglobaloverview.pdf')
+graph = nx.DiGraph()
+graph.add_edges_from(edge_list)
 
-print(normalize_text(x))
+def bfs(graph: dict, start: str) -> list:
+    """
+    BFS algorithm.
 
-# print(x.pages[1])
-# text = ''
-# marker = False
-# for page in x.pages:
-#     words = page.extract_text()
-#     if ('References' in words) or marker:
-#         marker = True
-#         text += words
+    :param graph: dict, A directed graph.
+    :param start: str, A start node.
+    :return: list, An ordered sequence of visited elements.
+    """
+    visited = []
+    queue = deque([start])
 
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            visited.append(node)
+            queue.extend(neighbor for neighbor in graph.neighbors(node) if neighbor not in visited)
+
+    return visited
+
+ITERATIONS = len(graph.nodes)
+START_RANK = 1 / ITERATIONS
+EPSILON = 0.01
+DAMPING_FACTOR = 0.85
+
+page_rank = {key: START_RANK for key in graph.nodes}
+page_prev = {key: 0 for key in graph.nodes}
+
+while True:
+    for node in page_rank:
+        current_rank = 0
+        reachable_nodes = bfs(graph, node)
+
+        for el in reachable_nodes:
+            if graph.has_edge(el, node):
+                out_links = graph.out_degree(el)
+
+                if out_links > 0:
+                    current_rank += page_rank[el] / out_links
+
+        page_prev[node] = (1 - DAMPING_FACTOR) + DAMPING_FACTOR * current_rank
+
+    diff = sum(abs(page_prev[node] - page_rank[node]) for node in graph.nodes)
+
+    if diff < EPSILON:
+        break
+
+    page_rank = dict(page_prev.items())
+
+page_rank = dict(sorted(page_rank.items(), key = lambda x: -x[1]))
+print(page_rank)
+nx.draw_circular(graph, with_labels = True)
+plt.show()
