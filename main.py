b9a2669355eaf162955a605e53dd2488c4bf18cc
Ostap Mnykh <mnykh.pn@ucu.edu.ua>
2024-12-01 19:12:11 +0200

    Changes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import os
 from collections import deque
 from functools import lru_cache
 import networkx as nx
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 from pdfminer.high_level import extract_text # lib to work with pdf files
 
@@ -15,7 +17,6 @@ PATTERN = re.compile(r'\[(.+?)\]\s+([\w.,\s&]+?),\s+(.*?),\s+(.*?)(?:\.|\n)') # 
 
 # edge_list = [('A', 'B'), ('B', 'D'), ('A', 'C'), ('C', 'D'), ('C', 'B'), ('C', 'A'), ('D', 'C')]
 
-@lru_cache
 def normalize_text(text):
     """Function to convert 'unique' elements to normal one"""
     normalized = unicodedata.normalize("NFKD", text)
@@ -93,41 +94,69 @@ def bfs(graph: dict, start: str) -> list:
 # print(pages_directions(
 #     read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf')))
 
-graph = nx.DiGraph()
-graph.add_edges_from(pages_directions(
-    read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf')))
+graph_rank = nx.DiGraph()
+# graph_rank.add_edges_from(pages_directions(
+#     read_pdf('test_papers/higherandderivedstacksaglobaloverview.pdf')))
 
+graph_rank.add_edges_from([('acharacterizationoffibrantsegalcategories', 'threemodelsforthehomotopytheoryofhomotopytheories'), ('higherandderivedstacksaglobaloverview', 'acharacterizationoffibrantsegalcategories'), ('higherandderivedstacksaglobaloverview', 'threemodelsforthehomotopytheoryofhomotopytheories'), ('threemodelsforthehomotopytheoryofhomotopytheories', 'acharacterizationoffibrantsegalcategories')])
 
-ITERATIONS = len(graph.nodes)
+ITERATIONS = len(graph_rank.nodes)
 START_RANK = 1 / ITERATIONS
 EPSILON = 0.01
 DAMPING_FACTOR = 0.85
 
-page_rank = {key: START_RANK for key in graph.nodes}
-page_prev = {key: 0 for key in graph.nodes}
+page_rank = {key: START_RANK for key in graph_rank.nodes}
+page_prev = {key: 0 for key in graph_rank.nodes}
 
 while True:
     for node in page_rank:
         current_rank = 0
-        reachable_nodes = bfs(graph, node)
+        reachable_nodes = bfs(graph_rank, node)
 
         for el in reachable_nodes:
-            if graph.has_edge(el, node):
-                out_links = graph.out_degree(el)
+            if graph_rank.has_edge(el, node):
+                out_links = graph_rank.out_degree(el)
 
                 if out_links > 0:
                     current_rank += page_rank[el] / out_links
 
         page_prev[node] = (1 - DAMPING_FACTOR) + DAMPING_FACTOR * current_rank
 
-    diff = [abs(page_prev[node] - page_rank[node])<EPSILON for node in graph.nodes]
+    diff = [abs(page_prev[node] - page_rank[node])<EPSILON for node in graph_rank.nodes]
 
     if all(diff):
         break
 
     page_rank = dict(page_prev.items())
 
+
 page_rank = dict(sorted(page_rank.items(), key = lambda x: -x[1]))
+
+# Normalize PageRank values for color mapping
+rank_values = np.array(list(page_rank.values()))
+norm_rank_values = (rank_values - rank_values.min()) / (rank_values.max() - rank_values.min())
+
+# Create a custom colormap from light blue to dark blue
+light_to_dark_blue = LinearSegmentedColormap.from_list("LightToDarkBlue", ["#add8e6", "#00008b"])
+
+# Create the figure and axis explicitly
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Draw the graph with circular layout
+pos = nx.circular_layout(graph_rank)
+nodes = nx.draw_networkx_nodes(
+    graph_rank, pos, node_color=norm_rank_values, cmap=light_to_dark_blue, node_size=800, ax=ax
+)
+edges = nx.draw_networkx_edges(graph_rank, pos, edge_color='gray', ax=ax)
+nx.draw_networkx_labels(graph_rank, pos, font_size=10, font_color='white', ax=ax)
+
+# Add a colorbar to indicate PageRank values
+sm = plt.cm.ScalarMappable(cmap=light_to_dark_blue, norm=plt.Normalize(vmin=rank_values.min(), vmax=rank_values.max()))
+sm.set_array([])
+cbar = fig.colorbar(sm, ax=ax, label='PageRank Score')
+
+# Display the graph
 print(page_rank)
-nx.draw_circular(graph, with_labels = True)
+nx.draw_circular(graph_rank, with_labels = True)
+
 plt.show()
